9cea403acd30526f5dda98afeb4c5381c118e8f4
David Heinemeier Hansson <david@37signals.com>
2025-04-18 16:32:27 +0200

    Make sure that any card getting read does not empty out the notifications tray
    Since we wont rehydrate it

--- app/controllers/notifications/trays_controller.rb
+++ app/controllers/notifications/trays_controller.rb
@@ -1,5 +1,5 @@
 class Notifications::TraysController < ApplicationController
   def show
-    @notifications = Current.user.notifications.unread.ordered.limit(20)
+    @notifications = Current.user.notifications.unread.ordered.limit(100)
   end
 end
