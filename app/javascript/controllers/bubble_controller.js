76119b17bdde15058b3526d600f195c16f6642d2
Jorge Manrubia <jorge@37signals.com>
2025-06-05 10:33:02 +0200

    Entropy, not bubble!

--- app/javascript/controllers/bubble_controller.js
+++ app/javascript/controllers/bubble_controller.js
@@ -4,7 +4,7 @@ import { signedDifferenceInDays } from "helpers/date_helpers"
 const REFRESH_INTERVAL = 3_600_000 // 1 hour (in milliseconds)
 
 export default class extends Controller {
-  static targets = [ "bubbleTop", "bubbleDays", "bubbleBottom" ]
+  static targets = [ "entropy", "entropyTop", "entropyDays", "entropyBottom" ]
   static values = { entropy: Object }
 
   #timer
@@ -26,9 +26,9 @@ export default class extends Controller {
       return
     }
 
-    this.bubbleTopTarget.innerHTML = closesInDays < 1 ? this.entropyValue.action : `${this.entropyValue.action} in`
-    this.bubbleDaysTarget.innerHTML = closesInDays < 1 ? "!" : closesInDays
-    this.bubbleBottomTarget.innerHTML = closesInDays < 1 ? "Today" : (closesInDays === 1 ? "day" : "days")
+    this.entropyTopTarget.innerHTML = closesInDays < 1 ? this.entropyValue.action : `${this.entropyValue.action} in`
+    this.entropyDaysTarget.innerHTML = closesInDays < 1 ? "!" : closesInDays
+    this.entropyBottomTarget.innerHTML = closesInDays < 1 ? "Today" : (closesInDays === 1 ? "day" : "days")
 
     this.#show()
   }
