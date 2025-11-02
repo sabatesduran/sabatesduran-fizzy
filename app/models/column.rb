a1543f2660c7e62151b40048cb8e84d4128fb170
David Heinemeier Hansson <david@hey.com>
2025-11-02 14:20:07 +0100

    Inline anemic helper method with no additional explaining purpose

--- app/models/column.rb
+++ app/models/column.rb
@@ -4,12 +4,7 @@ class Column < ApplicationRecord
   belongs_to :collection, touch: true
   has_many :cards, dependent: :nullify
 
-  before_validation :set_default_color
+  before_validation    -> { self.color ||= Card::DEFAULT_COLOR }
   after_save_commit    -> { cards.touch_all }, if: -> { saved_change_to_name? || saved_change_to_color? }
   after_destroy_commit -> { collection.cards.touch_all }
-
-  private
-    def set_default_color
-      self.color ||= Card::DEFAULT_COLOR
-    end
 end
