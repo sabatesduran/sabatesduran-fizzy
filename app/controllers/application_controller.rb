11b6bad14cd4f560d8ba70c61cf04a24f6ed4bed
Kevin McConnell <kevin@37signals.com>
2025-11-10 12:18:20 +0000

    Remove custom read/writer routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-  include LoadBalancerRouting, WriterAffinity
   include Authentication
   include Authorization
   include CurrentRequest, CurrentTimezone, SetPlatform
   include TurboFlash, ViewTransitions
   include Saas
+  include WriterAffinity
 
   stale_when_importmap_changes
   allow_browser versions: :modern
