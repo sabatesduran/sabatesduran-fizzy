009b1a0fec8664d3b7d62ae2467012e9cbd4497d
Rosa Gutierrez <rosa@37signals.com>
2026-01-05 12:10:57 +0100

    Take a first stab at offline mode support

--- app/javascript/initializers/index.js
+++ app/javascript/initializers/index.js
@@ -1,2 +1,3 @@
 import "initializers/current"
 import "initializers/bridge/bridge_element"
+import "initializers/offline"
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,6 +2,7 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/turbo/offline", to: "turbo-offline.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "@hotwired/hotwire-native-bridge", to: "@hotwired--hotwire-native-bridge.js"
